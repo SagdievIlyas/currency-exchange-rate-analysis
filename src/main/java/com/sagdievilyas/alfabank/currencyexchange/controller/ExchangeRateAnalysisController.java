1ed09fe2b9ab080c5383769961cb56a812b466bb
Ilyas <is@glonassmobile.com>
2022-06-07 10:27:16 +0300

    Fix

--- src/main/java/com/sagdievilyas/alfabank/currencyexchange/controller/ExchangeRateAnalysisController.java
+++ src/main/java/com/sagdievilyas/alfabank/currencyexchange/controller/ExchangeRateAnalysisController.java
@@ -9,7 +9,7 @@ import com.sagdievilyas.alfabank.currencyexchange.service.exchangeRateAnalysis.E
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExchangeRateAnalysisController {
     private final ExchangeRateAnalysisService exchangeRateAnalysisService;
 
-    @PostMapping("/create")
+    @GetMapping("/create")
     public ResponseEntity<?> createAnalysis(ExchangeRateAnalysisRequest request) {
         try {
             String resultGifUrl = exchangeRateAnalysisService.analyzeExchangeRate(request);
